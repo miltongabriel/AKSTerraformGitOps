071d16890348fc07918309ef302190dc7402984b
Milton Gabriel Lira Benetti <miltongabriel_9@hotmail.com>
2026-08-27 17:01:48 -0300

    fix: added permission to the aks pull image from acr

--- terraform/01-aks/aks.tf
+++ terraform/01-aks/aks.tf
@@ -35,3 +35,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     project     = var.project_name
   }
 }
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
