b34ca52b97b17fbf43b011851b48730b2e11b4cd
Milton Gabriel Lira Benetti <miltongabriel_9@hotmail.com>
2026-08-28 17:45:39 -0300

    ci: change made to trigger the pipeline

--- terraform/01-aks/outputs.tf
+++ terraform/01-aks/outputs.tf
@@ -1,3 +1,3 @@
 output "Access_credentials" {
-  value = "az aks get-credentials --resource-group rg-${var.project_name}-${var.environment}-${var.location} --name ${azurerm_kubernetes_cluster.aks.name}"
+  value = "az aks get-credentials --resource-group rg-${var.project_name}-${var.environment}-${var.location} --name ${azurerm_kubernetes_cluster.aks.name} "
 }
